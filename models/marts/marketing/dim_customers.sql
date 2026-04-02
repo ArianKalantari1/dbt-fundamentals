8206f65e13cc6a29ba63e003f33bf2a42a1bec8e
arian.kalantari21@gmail.com <arian.kalantari21@gmail.com>
2026-04-02 00:33:43 +0000

    added jaffle_shop source yaml file

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,39 +1,30 @@
-with
-    customers as (
-        select * from {{ ref('stg_jaffle_shop__customers') }}
-    ),
-    orders as (
-        select * from {{ ref('stg_jaffle_shop__orders') }}
-    ),
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
 
-    customer_orders as (
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
 
-        select
-            customer_id,
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
-        from orders
+order_payments as (
+    select
+        order_id,
+        sum(case when payment_status = 'success' then payment_amount end) as amount
 
-        group by 1
+    from payments
+    group by 1
+),
 
-    ),
+ final as (
 
-    final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
 
-        select
-            customers.customer_id,
-            customers.first_name,
-            customers.last_name,
-            customer_orders.first_order_date,
-            customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+    from orders
+    left join order_payments using (order_id)
+)
 
-        from customers
-
-        left join customer_orders using (customer_id)
-
-    )
-
-select *
-from final
+select * from final
